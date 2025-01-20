a1e6ebc625c2f1a36c072354d7c485db508137d7
rickyyuan07 <rickyyuan07@gmail.com>
2025-01-20 22:43:10 +0000

    Deploying to gh-pages from @ rickyyuan07/rickyyuan07.github.io@19302f8c4c125d8f2fcad6246667e0cbc2d6c80c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Please also check out pdf version and Linkedin as well.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
